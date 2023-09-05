7e0a705a9ac25128f914b16240854548c02b0415
GrkmOzkr <gorkemozkur@gmail.com>
2023-09-05 03:49:01 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,13 +3,13 @@ const { AccessToken } = require('livekit-server-sdk');
 const cors = require('cors');
 const bodyParser = require('body-parser'); // Import body-parser
 
-const createToken = () => {
+const createToken = () => 
   // if this room doesn't exist, it'll be automatically created when the first
   // client joins
-  return new AccessToken('APIB7mkxVfcNXHo', 'hBPONEbtMxVqcvsNSLKIUDf0ONWiBhaC1E4JCd8Ai3F', {
+   new AccessToken('APIB7mkxVfcNXHo', 'hBPONEbtMxVqcvsNSLKIUDf0ONWiBhaC1E4JCd8Ai3F', {
     identity: 'quickstart-username',
-  }).toJwt();
-}
+  }).toJwt()
+
 
 const app = express();
 const port = 3000;
