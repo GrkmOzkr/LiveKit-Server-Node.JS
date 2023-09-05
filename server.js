ab3632c74179d9b9c0bcdb1108b6e5d40156f647
GrkmOzkr <gorkemozkur@gmail.com>
2023-09-05 03:41:40 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,28 +1,20 @@
-// server.js
 const express = require('express');
 const { AccessToken } = require('livekit-server-sdk');
 const cors = require('cors');
 const bodyParser = require('body-parser'); // Import body-parser
 
-
 const createToken = () => {
   // if this room doesn't exist, it'll be automatically created when the first
   // client joins
-  const roomName = 'quickstart-room';
-  // identifier to be used for participant.
-  // it's available as LocalParticipant.identity with livekit-client SDK
-  const participantName = 'quickstart-username';
-
-  const at = new AccessToken('APIB7mkxVfcNXHo', 'hBPONEbtMxVqcvsNSLKIUDf0ONWiBhaC1E4JCd8Ai3F', {
-    identity: participantName,
-  });
-  at.addGrant({ roomJoin: true, room: roomName });
-
-  return at.toJwt();
+  return new AccessToken('APIB7mkxVfcNXHo', 'hBPONEbtMxVqcvsNSLKIUDf0ONWiBhaC1E4JCd8Ai3F', {
+    identity: 'quickstart-username',
+  }).toJwt();
 }
 
 const app = express();
 const port = 3000;
+
+app.use(bodyParser.json()); // JSON parsing middleware
 
 app.use(cors({
   origin: '*',
@@ -35,22 +27,14 @@ app.get('/', (req, res) => {
 
 // Modify this route to accept POST requests
 app.post('/getToken', (req, res) => {
-  // Handle POST request data here
-  // You can access the JSON data sent in the request body using req.body
-  // For example, you can parse it and return a response
-  const requestData = req.body;
+  console.log('Received POST request to /getToken');
 
-  res.send(createToken());
+  // You can remove this line if requestData is not used
+  // const requestData = req.body;
 
-  // Call your createToken function with requestData if needed
-  // const token = createToken(requestData);
-
-  // Send a response, for example:
-  // res.json({ token });
-
-  // Don't forget to handle errors and validation
+  res.json({ token: createToken() });
 });
 
 app.listen(port, () => {
-  console.log(`Server listening on port ${port}`)
-})
+  console.log(`Server listening on port ${port}`);
+});
